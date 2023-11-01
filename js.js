3af891d6a73009a2ee0d0d56ad0f4101fe33ef76
JoaoGustavoDEVSenai <133894702+JoaoGustavoDEVSenai@users.noreply.github.com>
2023-11-01 12:29:16 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,45 @@
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}
+
+
+//2
+function allowDrop2(ev) {
+    ev.preventDefault();
+  }
+  
+  function drag2(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+  }
+  
+  function drop2(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+  }
+
+
+//3
+  function allowDrop3(ev) {
+    ev.preventDefault();
+  }
+  
+  function drag3(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+  }
+  
+  function drop3(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+  }
